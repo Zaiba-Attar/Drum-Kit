d757653ddcb8238c01b0ea7fa2909d10b56f6cb6
Zaiba Attar <74246426+Zaiba-Attar@users.noreply.github.com>
2021-10-03 18:46:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,31 +27,31 @@ function keyBoardClick(event){
 function makeSound(key){
     switch (key) {
         case "w":
-            var audio = new Audio ("sounds/tom-1.mp3");    //assigning sounds to each button accoding to their text in the innerhtml
+            var audio = new Audio ("tom-1.mp3");    //assigning sounds to each button accoding to their text in the innerhtml
             audio.play();
             break;
         case "a":
-            var audio = new Audio ("sounds/tom-2.mp3");   //assigning sounds to each button accoding to their text in the innerhtml
+            var audio = new Audio ("tom-2.mp3");   //assigning sounds to each button accoding to their text in the innerhtml
             audio.play();
             break;
         case "s":
-            var audio = new Audio ("sounds/tom-3.mp3");   //assigning sounds to each button accoding to their text in the innerhtml
+            var audio = new Audio ("tom-3.mp3");   //assigning sounds to each button accoding to their text in the innerhtml
             audio.play();
             break;
         case "d":
-            var audio = new Audio ("sounds/tom-4.mp3");  //assigning sounds to each button accoding to their text in the innerhtml 
+            var audio = new Audio ("tom-4.mp3");  //assigning sounds to each button accoding to their text in the innerhtml 
             audio.play();
             break;
         case "j":
-            var audio = new Audio ("sounds/snare.mp3");  //assigning sounds to each button accoding to their text in the innerhtml
+            var audio = new Audio ("snare.mp3");  //assigning sounds to each button accoding to their text in the innerhtml
             audio.play();
             break;
         case "k":
-            var audio = new Audio ("sounds/kick-bass.mp3"); //assigning sounds to each button accoding to their text in the innerhtml 
+            var audio = new Audio ("kick-bass.mp3"); //assigning sounds to each button accoding to their text in the innerhtml 
             audio.play();
             break;
         case "l":
-            var audio = new Audio ("sounds/crash.mp3"); //assigning sounds to each button accoding to their text in the innerhtml
+            var audio = new Audio ("crash.mp3"); //assigning sounds to each button accoding to their text in the innerhtml
             audio.play();
             break;
         default:
